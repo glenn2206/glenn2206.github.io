ec96874564e67f8f68292135e32af400f6b37b3f
glenn2206 <leonardusglenn@gmail.com>
2021-10-21 15:14:28 +0700

    Glenn

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
+Pembagian Tugas Hangman
+1. Cari database untuk kata dan pertanyaannya (Andre, Glenn)
+  1. buat database dulu -> array ["kategori",["kata-kata"]]
+  2. 
+
+
+2. Tampilan (Luga,David, Putra )
+  1. Loadear -> start untuk mulai
+  2. Page 2 -> pilihan kategori
+  3. Game mulai
+    -> kesempatan
+  4. Animasi solved
+  5. Animasi salah 
+3. 
+test123
+
+
+ABSENSI!!
+1. GLENN
+a
+
+
+*/
+
+let database = ["kategori"]
+console.log(database)
